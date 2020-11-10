933594aa0c4aae97daded78aca040fb247811db6
thesuhas <suhastvs@gmail.com>
2020-11-10 16:34:30 +0530

    Set up links to login and register using react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 {/* If only single element is being returned, no need of curly braces*/}
 {/* Fragment are ghost elements, won't show up in dom */}
 
-const App = () => 
-    <Fragment>
-      <Navbar/>
-      <Landing/>
-    </Fragment>
+const App = () => (
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Route exact path="/" component= {Landing}/>
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/login" component={Login}/>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+);
 
 export default App;
