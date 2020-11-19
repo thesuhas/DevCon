3966287b4ef923709d19452b95454a459992ff58
thesuhas <suhastvs@gmail.com>
2020-11-19 21:40:17 +0530

    Made CreateProfile as well as form

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import {loadUser} from './actions/auth';
 import './App.css';
 // Redux
@@ -36,6 +37,7 @@ const App = () => {
 				<Route exact path="/register" component={Register}/>
 				<Route exact path="/login" component={Login}/>
 				<PrivateRoute exact path="/dashboard" component={Dashboard}/>
+				<PrivateRoute exact path="/create-profile" component={CreateProfile}/>
 			</Switch>
 			</section>
 		</Fragment>
