298f49e9cd43bf39bae5ff921c208c1df82fab0d
thesuhas <suhastvs@gmail.com>
2020-11-20 18:39:48 +0530

    Create profile action setup

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,43 @@ export const getCurrentProfile =  () => async dispatch => {
         });
     }
 };
+
+// Create or Update Profile
+export const createProfile = (formData, history, edit=false) => async dispatch => {// History object has a method called push that can help us redirect to a client side route
+    try {
+        // Need to create config object to send data
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.post('/api/profile', formData, config);
+
+        dispatch ({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", 'sucess')); // If edit is true, send alert that profile has been updated, else created
+
+        // If creating profile, redirect
+        if (!edit) {
+            history.push('/dashboard'); // Cannot redirect normally in an action
+        }
+
+    } catch(err){
+        const errors = err.response.data.errors; // Gets body errors
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+
+    }
+
+};
