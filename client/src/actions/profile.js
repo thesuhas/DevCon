8283a8c70b84e119576d72f3a082559f4084ce64
thesuhas <suhastvs@gmail.com>
2020-11-21 17:26:59 +0530

    Finished necessary profile actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import {CLEAR_PROFILE, GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED} from './types';
+import {CLEAR_PROFILE, GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED, GET_PROFILES, GET_REPOS} from './types';
 
 // Get current users profile
 export const getCurrentProfile =  () => async dispatch => {
@@ -10,6 +10,61 @@ export const getCurrentProfile =  () => async dispatch => {
     
         dispatch ({
             type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
+
+// Get all profiles
+export const getProfiles =  () => async dispatch => {
+    dispatch({type: CLEAR_PROFILE}); // Clearing profile before you visit the page prevents the flashing of previous users profile
+    try {
+        // Will get profile from the user id
+        const res = await axios.get('/api/profile');
+    
+        dispatch ({
+            type: GET_PROFILES,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
+
+// Get a particular profile by USER ID
+export const getProfileById =  userId => async dispatch => {
+    try {
+        // Will get profile from the user id
+        const res = await axios.get(`/api/profile/user/${userId}`);
+    
+        dispatch ({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
+
+// Get GitHub Repos
+export const getGithubRepos =  username => async dispatch => {
+    try {
+        // Will get profile from the user id
+        const res = await axios.get(`/api/profile/github/${username}`);
+    
+        dispatch ({
+            type: GET_REPOS,
             payload: res.data
         });
     } catch (err) {
