aa29ad277c8dd39efaa0a13f2ba555774fe8384e
thesuhas <suhastvs@gmail.com>
2020-11-23 23:07:47 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // Importing express
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Initialising app variable
 const app = express();
@@ -13,13 +14,23 @@ connectDB();
 app.use(express.json({extended: false}));
 
 // Creating get request that send data to browser for test
-app.get('/', (req, res) => res.send('API Running')); // Send a response stating that the API is running
+//app.get('/', (req, res) => res.send('API Running')); // Send a response stating that the API is running
 
 // Define routs
 app.use('/api/users', require('./routes/api/users')); // Makes api/users pertain to the get request in users.js
 app.use('/api/profile', require('./routes/api/profile')); // Makes api/users pertain to the get request in profile.js
 app.use('/api/auth', require('./routes/api/auth')); // Makes api/users pertain to the get request in auth.js
 app.use('/api/posts', require('./routes/api/posts')); // Makes api/users pertain to the get request in posts.js
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // Listening on a port
 const PORT = process.env.PORT || 5000; // Looks at environment variable called port, if not set will go to 5000 
